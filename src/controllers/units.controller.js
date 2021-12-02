5f645372bada2fa5621999779ff296c05e8d8e0d
Mike Keen <mwk@mikekeen.com>
2021-12-02 02:12:28 +0000

    feat: data model, stubs and test for units

--- src/controllers/units.controller.js
+++ src/controllers/units.controller.js
@@ -1,17 +1,25 @@
+import { Unit, UnitMock } from '../models';
+
 export const create = (req, res) => {
   res.json({
     message: 'Not Yet Implemented',
   });
 };
-export const findAll = (req, res) => {
-  res.json({
-    message: 'Not Yet Implemented',
-  });
+export const findAll = async (req, res) => {
+  if (req.query.useMock) {
+    res.json(UnitMock.findAll());
+    return;
+  }
+
+  res.json(await Unit.findAll());
 };
-export const findOne = (req, res) => {
-  res.json({
-    message: 'Not Yet Implemented',
-  });
+export const findOne = async (req, res) => {
+  if (req.query.useMock) {
+    res.json(UnitMock.findOne(req.query.id));
+    return;
+  }
+
+  res.json(await Unit.findByPk(req.query.id));
 };
 export const update = (req, res) => {
   res.json({
