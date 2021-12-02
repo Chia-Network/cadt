f68bedfe1c535b7c395dd34669a14310597a5ad0
Michael.Taylor <mtaylor@michaeltaylor3d.com>
2021-12-02 17:29:11 -0500

    feat: add stubs and mocks for all resources

--- src/controllers/location.controller.js
+++ src/controllers/location.controller.js
@@ -1,23 +1,45 @@
+import { LocationMock } from '../models';
+
 export const create = (req, res) => {
   res.json({
     message: 'Not Yet Implemented',
   });
 };
+
 export const findAll = (req, res) => {
+  if (req.query.useMock) {
+    res.json(LocationMock.findAll());
+    return;
+  }
+
   res.json({
     message: 'Not Yet Implemented',
   });
 };
+
 export const findOne = (req, res) => {
+  if (req.query.useMock) {
+    const record = LocationMock.findOne(req.query.id);
+    if (record) {
+      res.json(record);
+    } else {
+      res.json({ message: 'Not Found' });
+    }
+
+    return;
+  }
+
   res.json({
     message: 'Not Yet Implemented',
   });
 };
+
 export const update = (req, res) => {
   res.json({
     message: 'Not Yet Implemented',
   });
 };
+
 export const destroy = (req, res) => {
   res.json({
     message: 'Not Yet Implemented',
--- src/controllers/rating.controller.js
+++ src/controllers/rating.controller.js
@@ -1,23 +1,45 @@
+import { RatingMock } from '../models';
+
 export const create = (req, res) => {
   res.json({
     message: 'Not Yet Implemented',
   });
 };
+
 export const findAll = (req, res) => {
+  if (req.query.useMock) {
+    res.json(RatingMock.findAll());
+    return;
+  }
+
   res.json({
     message: 'Not Yet Implemented',
   });
 };
+
 export const findOne = (req, res) => {
+  if (req.query.useMock) {
+    const record = RatingMock.findOne(req.query.id);
+    if (record) {
+      res.json(record);
+    } else {
+      res.json({ message: 'Not Found' });
+    }
+
+    return;
+  }
+
   res.json({
     message: 'Not Yet Implemented',
   });
 };
+
 export const update = (req, res) => {
   res.json({
     message: 'Not Yet Implemented',
   });
 };
+
 export const destroy = (req, res) => {
   res.json({
     message: 'Not Yet Implemented',
