4ca5cd7f6616631347dae9d8dca9c4a49ce790bb
Mike Keen <mwk@mikekeen.com>
2021-12-02 00:39:42 +0000

    improvement: remove done from qual spec

--- tests/routes/qualifications.spec.js
+++ tests/routes/qualifications.spec.js
@@ -9,24 +9,22 @@ chai.should();
 
 describe("Qualifications Routes", () => {
   describe("GET /v1/qualifications/", () => {
-    it("should get all qualifications", (done) => {
+    it("should get all qualifications", () => {
       chai.request(app)
         .get('/v1/qualifications/?useMock=true')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('array');
-          done();
         });
     });
   });
   describe("GET /v1/qualifications/?id=1", () => {
-    it("should get a single qualification", (done) => {
+    it("should get a single qualification", () => {
       chai.request(app)
         .get('/v1/qualifications/?useMock=true&id=1')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
-          done();
         });
     });
   });
