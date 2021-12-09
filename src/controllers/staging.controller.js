657b34a33f6e14afe224104bc1f18377860b4942
Frantz Arty <artyfrantz@gmail.com>
2021-12-09 15:44:36 -0500

    feat: add diffs to stage resource

--- src/controllers/staging.controller.js
+++ src/controllers/staging.controller.js
@@ -1,4 +1,6 @@
 import { Staging, StagingMock } from '../models';
+import _ from 'lodash';
+import { Staging, StagingMock, Project, Unit } from '../models';
 
 export const findAll = async (req, res) => {
   if (req.query.useMock) {
@@ -6,7 +8,38 @@ export const findAll = async (req, res) => {
     return;
   }
 
-  res.json(Staging.findAll());
+  const stagingData = await Staging.findAll();
+
+  const response = await Promise.all(
+    stagingData.map(async (data) => {
+      const workingData = _.cloneDeep(data.dataValues);
+      workingData.diff = {};
+      if (workingData.action === 'INSERT') {
+        workingData.diff.original = {};
+        workingData.diff.change = JSON.parse(workingData.data);
+      }
+
+      if (workingData.action === 'UPDATE') {
+        const Model = workingData.table === 'Projects' ? Project : Unit;
+        const original = await Model.findOne({
+          where: { uuid: workingData.uuid },
+        });
+        workingData.diff.original = original;
+        workingData.diff.change = JSON.parse(workingData.data);
+      }
+
+      if (workingData.action === 'DELETE') {
+        workingData.diff.original = JSON.parse(workingData.data);
+        workingData.diff.change = {};
+      }
+
+      delete workingData.data;
+
+      return workingData;
+    }),
+  );
+
+  res.json(response);
 };
 
 export const destroy = (req, res) => {
--- src/routes/v1/resources/stagings.js
+++ src/routes/v1/resources/stagings.js
@@ -2,6 +2,11 @@
 
 import express from 'express';
 import { StagingController } from './staging./../../controllers';
+import Joi from 'joi';
+import joiExpress from 'express-joi-validation';
+import { StagingController } from '../../../controllers';
+
+const validator = joiExpress.createValidator({});
 const StagingRouter = express.Router();
 
 StagingRouter.get('/', (req, res) => {
