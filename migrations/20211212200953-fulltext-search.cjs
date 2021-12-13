e5297ed34462070a7cef6c36e852b981299a7916
Mike Keen <mwk@mikekeen.com>
2021-12-13 10:56:08 -0500

    feat: fts on projects and units

---
+++ migrations/20211212200953-fulltext-search.cjs
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query("create virtual table Projects using fts5 (warehouseProjectId, currentRegistry, registryOfOrigin, originProjectId, program, projectName, projectLink, projectDeveloper, sector, projectType, coveredByNDC, NDCLinkage, projectStatus, projectStatusDate, unitMetric, methodology, methodologyVersion, validationApproach, validationDate, projectTag, estimatedAnnualAverageEmissionReduction, owner);");
+    await queryInterface.sequelize.query("create virtual table Units using fts5 (ProjectId, owner, buyer, registry, blockIdentifier, identifier, qualificationId, unitType, unitCount, unitStatus, unitStatusDate, transactionType, unitIssuanceLocation, unitLink, correspondingAdjustment, unitTag, vintageId");
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
