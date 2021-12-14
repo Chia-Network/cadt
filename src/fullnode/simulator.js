d87d7c7d60b8726b9ec3b1e367900832d3b1a062
Frantz Arty <artyfrantz@gmail.com>
2021-12-14 17:49:42 -0500

    feat: add simulator

---
+++ src/fullnode/simulator.js
@@ -0,0 +1,64 @@
+import { Project, Unit } from '../models';
+const THIRTY_SEC = 30000;
+
+// Simulate 30 seconds passing before commited to node
+
+export const updateProjectRecord = async (uuid, record) => {
+  await deleteProjectRecord(uuid);
+  await createProjectRecord(uuid, record);
+};
+
+export const createProjectRecord = (uuid, record) => {
+  return new Promise(async (resolve) => {
+    setTimeout(() => {
+      await Project.create({
+        ...record,
+        warehouseProjectId: uuid,
+      });
+      resolve();
+    }, THIRTY_SEC);
+  });
+};
+
+export const deleteProjectRecord = (uuid) => {
+  return new Promise(async (resolve) => {
+    setTimeout(() => {
+      await Project.destroy({
+        where: {
+          warehouseProjectId: uuid,
+        },
+      });
+      resolve();
+    }, THIRTY_SEC);
+  });
+};
+
+export const updateUnitRecord = async (uuid, record) => {
+  await deleteUnitRecord(uuid);
+  await createUnitRecord(uuid, record);
+};
+
+export const createUnitRecord = (uuid, record) => {
+  return new Promise(async (resolve) => {
+    setTimeout(() => {
+      await Unit.create({
+        uuid,
+        ...record,
+      });
+      resolve();
+    }, THIRTY_SEC);
+  });
+};
+
+export const deleteUnitRecord = (uuid) => {
+  return new Promise(async (resolve) => {
+    setTimeout(() => {
+      await Unit.destroy({
+        where: {
+          uuid,
+        },
+      });
+      resolve();
+    }, THIRTY_SEC);
+  });
+};
