ef451f850b4a6ffde20981fab15588fbe523f2e4
Mike Keen <mwk@mikekeen.com>
2021-12-19 14:13:01 -0500

    feat: triggers for fts on units and projects in sqlite

--- migrations/20211212200953-fulltext-search.cjs
+++ migrations/20211212200953-fulltext-search.cjs
@@ -4,56 +4,18 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     if (queryInterface.sequelize.getDialect() === 'sqlite') {
       await queryInterface.sequelize.query(`
-      create virtual table projects_fts using fts5 (
-        warehouseProjectId, 
-        currentRegistry, 
-        registryOfOrigin,
-        originProjectId, 
-        program, 
-        projectName, 
-        projectLink, 
-        projectDeveloper,
-        sector, 
-        projectType, 
-        coveredByNDC, 
-        NDCLinkage, 
-        projectStatus,
-        projectStatusDate, 
-        unitMetric, 
-        methodology, 
-        methodologyVersion,
-        validationApproach, 
-        validationDate,
-         projectTag,
-        estimatedAnnualAverageEmissionReduction, 
-        );`);
-      await queryInterface.sequelize.query(
-        `create virtual table units_fts using fts5 (
-          projectId, 
-          buyer, 
-          registry, 
-          blockIdentifier, 
-          identifier,
-          qualificationId, 
-          unitType, 
-          unitCount, 
-          unitStatus, 
-          unitStatusDate,
-          transactionType, 
-          unitIssuanceLocation,
-          unitLink, 
-          correspondingAdjustment,
-          unitTag, 
-          vintageId
-        );`,
-      );
+      CREATE VIRTUAL TABLE projects_fts USING fts5(projects);
+      `);
+      await queryInterface.sequelize.query(`
+      CREATE VIRTUAL TABLE units_fts USING fts5(units);
+      `);
     }
   },
 
   down: async (queryInterface, Sequelize) => {
     if (queryInterface.sequelize.getDialect() === 'sqlite') {
-      await queryInterface.sequelize.query(`drop virtual table projects_fts;`);
-      await queryInterface.sequelize.query('drop virtual table units_fts;');
+      await queryInterface.sequelize.query(`drop table projects_fts;`);
+      await queryInterface.sequelize.query('drop table units_fts;');
     }
   },
 };
---
+++ migrations/20211219184405-sqlite-triggers-units.cjs
@@ -0,0 +1,105 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    if (queryInterface.sequelize.getDialect() === 'sqlite') {
+      await queryInterface.sequelize.query(`
+      CREATE TRIGGER unit_insert_fts AFTER INSERT ON units BEGIN
+        INSERT INTO units_fts(
+          id,
+          projectId, 
+          buyer, 
+          registry, 
+          blockIdentifier, 
+          identifier,
+          qualificationId, 
+          unitType, 
+          unitCount, 
+          unitStatus, 
+          unitStatusDate,
+          transactionType, 
+          unitIssuanceLocation,
+          unitLink, 
+          correspondingAdjustment,
+          unitTag, 
+          vintageId
+        ) VALUES (
+          new.id,
+          new.projectId, 
+          new.buyer, 
+          new.registry, 
+          new.blockIdentifier, 
+          new.identifier,
+          new.qualificationId, 
+          new.unitType, 
+          new.unitCount, 
+          new.unitStatus, 
+          new.unitStatusDate,
+          new.transactionType, 
+          new.unitIssuanceLocation,
+          new.unitLink, 
+          new.correspondingAdjustment,
+          new.unitTag, 
+          new.vintageId
+        );
+      END;`);
+
+      await queryInterface.sequelize.query(`
+      CREATE TRIGGER unit_delete_fts AFTER DELETE ON units BEGIN
+        DELETE FROM unit_insert_fts WHERE id = old.id;
+      END;
+      `);
+
+      await queryInterface.sequelize.query(`
+      CREATE TRIGGER unit_update_fts AFTER UPDATE ON units BEGIN
+        DELETE FROM units_fts WHERE id = old.id;
+        INSERT INTO units_fts(
+          id,
+          projectId,
+          buyer,
+          registry,
+          blockIdentifier,
+          identifier,
+          qualificationId,
+          unitType,
+          unitCount,
+          unitStatus,
+          unitStatusDate,
+          transactionType,
+          unitIssuanceLocation,
+          unitLink,
+          correspondingAdjustment,
+          unitTag,
+          vintageId
+        ) VALUES (
+          new.id,
+          new.projectId,
+          new.buyer,
+          new.registry,
+          new.blockIdentifier,
+          new.identifier,
+          new.qualificationId,
+          new.unitType,
+          new.unitCount,
+          new.unitStatus,
+          new.unitStatusDate,
+          new.transactionType,
+          new.unitIssuanceLocation,
+          new.unitLink,
+          new.correspondingAdjustment,
+          new.unitTag,
+          new.vintageId
+        );
+      END;
+      `);
+    }
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    if (queryInterface.sequelize.getDialect() === 'sqlite') {
+      await queryInterface.sequelize.query("DROP TRIGGER unit_insert_fts;");
+      await queryInterface.sequelize.query("DROP TRIGGER unit_delete_fts;");
+      await queryInterface.sequelize.query("DROP TRIGGER unit_update_fts;");
+    }
+  }
+};
