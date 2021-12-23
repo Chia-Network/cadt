3e84f0e63d63d8e708b40f24d785cd2eb45ca259
Mike Keen <mwk@mikekeen.com>
2021-12-23 01:06:19 -0500

    helpers

---
+++ src/controllers/helpers.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export const paginationParams = (page, limit) => {
+  if (page < 1) {
+    page = 1;
+  }
+
+  if (limit < 1) {
+    limit = 1;
+  }
+
+  return {
+    limit: limit ? limit : 15,
+    offset: page ? page - 1 : 1,
+  }
+}
