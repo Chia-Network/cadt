c56fccf45118ecb695db58684a38a60dae3c514f
Frantz Arty <artyfrantz@gmail.com>
2022-01-11 13:19:18 -0500

    feat: Created unit integration test for delete

--- tests/integration/unit.spec.js
+++ tests/integration/unit.spec.js
@@ -7,7 +7,103 @@ import { exec } from 'child_process';
 import { WAIT_TIME } from '../../src/fullnode/simulator';
 
 const { expect } = chai;
+const createUnit = async () => {
+  // 1. Create a new unit
+  // 2. verify the unit is in the staging tables
+  // 3. verify the inferred data has been added to the unit record
+  // 3. commit the staging tables
+  // 4. verify that the staging table has the committed flag set to true
+  // 5. verify the unit was commited to the database
+  // 6. verify the staging table was cleaned up
 
+  const payload = {
+    unitBlockStart: 'AXJJFSLGHSHEJ1000',
+    unitBlockEnd: 'AXJJFSLGHSHEJ1010',
+    countryJuridictionOfOwner: 'USA',
+    inCountryJuridictionOfOwner: 'Maryland',
+    tokenIssuanceHash: '0x7777',
+  };
+  const unitRes = await supertest(app).post('/v1/units').send(payload);
+
+  expect(unitRes.statusCode).to.equal(200);
+  expect(unitRes.body).to.deep.equal({
+    message: 'Unit created successfully',
+  });
+
+  // Get the organizations so we can check the right org was set
+  const organizationResults = await supertest(app).get('/v1/organizations');
+  const orgUid = Object.keys(organizationResults.body).find(
+    (key) => organizationResults.body[key].writeAccess,
+  );
+
+  // Get the staging record we just created
+  const stagingRes = await supertest(app).get('/v1/staging');
+  const stagingRecord = _.head(stagingRes.body);
+
+  // There is no original when creating new units
+  expect(stagingRecord.diff.original).to.deep.equal({});
+
+  const changeRecord = _.head(stagingRecord.diff.change);
+
+  // make sure the inferred data was set to the staging record
+  expect(changeRecord.orgUid).to.equal(orgUid);
+  expect(changeRecord.unitOwnerOrgUid).to.equal(orgUid);
+
+  const warehouseUnitId = changeRecord.warehouseUnitId;
+
+  expect(stagingRes.statusCode).to.equal(200);
+
+  // Now push the staging table live
+  const commitRes = await supertest(app).post('/v1/staging/commit');
+  expect(stagingRes.statusCode).to.equal(200);
+  expect(commitRes.body).to.deep.equal({
+    message: 'Staging Table committed to full node',
+  });
+
+  // After commiting the true flag should be set to this staging record
+  const stagingRes2 = await supertest(app).get('/v1/staging');
+  expect(_.head(stagingRes2.body).commited).to.equal(true);
+
+  // The node simulator runs on an async process, we are importing
+  // the WAIT_TIME constant from the simulator, padding it and waiting for the
+  // appropriate amount of time for the simulator to finish its operations
+  await new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      try {
+        const warehouseRes = await supertest(app)
+          .get(`/v1/units`)
+          .query({ warehouseUnitId });
+
+        const newRecord = warehouseRes.body;
+
+        expect(newRecord.warehouseUnitId).to.equal(warehouseUnitId);
+        expect(newRecord.orgUid).to.equal(orgUid);
+        expect(newRecord.unitOwnerOrgUid).to.equal(orgUid);
+        expect(newRecord.unitBlockStart).to.equal(payload.unitBlockStart);
+        expect(newRecord.unitBlockEnd).to.equal(payload.unitBlockEnd);
+        expect(newRecord.countryJuridictionOfOwner).to.equal(
+          payload.countryJuridictionOfOwner,
+        );
+        expect(newRecord.inCountryJuridictionOfOwner).to.equal(
+          payload.inCountryJuridictionOfOwner,
+        );
+        expect(newRecord.tokenIssuanceHash).to.equal(payload.tokenIssuanceHash);
+
+        // Make sure the staging table is cleaned up
+        const stagingRes3 = await supertest(app).get('/v1/staging');
+
+        // There should be no staging records left
+        expect(stagingRes3.body.length).to.equal(0);
+
+        resolve();
+      } catch (err) {
+        reject(err);
+      }
+    }, WAIT_TIME + 1000);
+  });
+
+  return { warehouseUnitId };
+};
 describe('Create Unit Integration', () => {
   beforeEach(async () => {
     await supertest(app).get(`/v1/staging/clean`);
@@ -224,100 +320,15 @@ describe('Create Unit Integration', () => {
   });
 
   it('creates a new unit end-to-end', async () => {
-    // 1. Create a new unit
-    // 2. verify the unit is in the staging tables
-    // 3. verify the inferred data has been added to the unit record
-    // 3. commit the staging tables
-    // 4. verify that the staging table has the committed flag set to true
-    // 5. verify the unit was commited to the database
-    // 6. verify the staging table was cleaned up
+    await createUnit();
+  });
 
-    const payload = {
-      unitBlockStart: 'AXJJFSLGHSHEJ1000',
-      unitBlockEnd: 'AXJJFSLGHSHEJ1010',
-      countryJuridictionOfOwner: 'USA',
-      inCountryJuridictionOfOwner: 'Maryland',
-      tokenIssuanceHash: '0x7777',
-    };
-    const unitRes = await supertest(app).post('/v1/units').send(payload);
+  it('deletes a unit end-to-end', async () => {
+    const { warehouseUnitId } = await createUnit();
 
-    expect(unitRes.statusCode).to.equal(200);
-    expect(unitRes.body).to.deep.equal({
-      message: 'Unit created successfully',
-    });
-
-    // Get the organizations so we can check the right org was set
-    const organizationResults = await supertest(app).get('/v1/organizations');
-    const orgUid = Object.keys(organizationResults.body).find(
-      (key) => organizationResults.body[key].writeAccess,
-    );
-
-    // Get the staging record we just created
-    const stagingRes = await supertest(app).get('/v1/staging');
-    const stagingRecord = _.head(stagingRes.body);
-
-    // There is no original when creating new units
-    expect(stagingRecord.diff.original).to.deep.equal({});
-
-    const changeRecord = _.head(stagingRecord.diff.change);
-
-    // make sure the inferred data was set to the staging record
-    expect(changeRecord.orgUid).to.equal(orgUid);
-    expect(changeRecord.unitOwnerOrgUid).to.equal(orgUid);
-
-    const warehouseUnitId = changeRecord.warehouseUnitId;
-
-    expect(stagingRes.statusCode).to.equal(200);
-
-    // Now push the staging table live
-    const commitRes = await supertest(app).post('/v1/staging/commit');
-    expect(stagingRes.statusCode).to.equal(200);
-    expect(commitRes.body).to.deep.equal({
-      message: 'Staging Table committed to full node',
-    });
-
-    // After commiting the true flag should be set to this staging record
-    const stagingRes2 = await supertest(app).get('/v1/staging');
-    expect(_.head(stagingRes2.body).commited).to.equal(true);
-
-    // The node simulator runs on an async process, we are importing
-    // the WAIT_TIME constant from the simulator, padding it and waiting for the
-    // appropriate amount of time for the simulator to finish its operations
-    await new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          const warehouseRes = await supertest(app)
-            .get(`/v1/units`)
-            .query({ warehouseUnitId });
-
-          const newRecord = warehouseRes.body;
-
-          expect(newRecord.warehouseUnitId).to.equal(warehouseUnitId);
-          expect(newRecord.orgUid).to.equal(orgUid);
-          expect(newRecord.unitOwnerOrgUid).to.equal(orgUid);
-          expect(newRecord.unitBlockStart).to.equal(payload.unitBlockStart);
-          expect(newRecord.unitBlockEnd).to.equal(payload.unitBlockEnd);
-          expect(newRecord.countryJuridictionOfOwner).to.equal(
-            payload.countryJuridictionOfOwner,
-          );
-          expect(newRecord.inCountryJuridictionOfOwner).to.equal(
-            payload.inCountryJuridictionOfOwner,
-          );
-          expect(newRecord.tokenIssuanceHash).to.equal(
-            payload.tokenIssuanceHash,
-          );
-
-          // Make sure the staging table is cleaned up
-          const stagingRes3 = await supertest(app).get('/v1/staging');
-
-          // There should be no staging records left
-          expect(stagingRes3.body.length).to.equal(0);
-
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
-      }, WAIT_TIME + 1000);
-    });
+    await supertest(app)
+      .del('/v1/units')
+      .send({ warehouseUnitId })
+      .expect(200, { message: 'Unit deleted successfully' });
   });
 });
