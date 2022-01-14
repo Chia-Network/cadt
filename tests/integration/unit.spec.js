4fc5345310fc26f18b861bb578545b8bcfe5e3cc
Michael Taylor <mtaylor@michaeltaylor.dev>
2022-01-13 21:08:52 -0500

    chore: eslint config

--- tests/integration/unit.spec.js
+++ tests/integration/unit.spec.js
@@ -192,7 +192,6 @@ describe('Create Unit Integration', () => {
 
     // There should be no staging records left
     expect(stagingRes3.body.length).to.equal(0);
-    expect(stagingRes3.body.length).to.equal(0);
   });
 
   it('creates a new unit end-to-end', async () => {
