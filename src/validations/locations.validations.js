f73fc4f8131072d2fac7a64e02ac9fb2e70330fb
Craig Cornick <98057517+ccornick@users.noreply.github.com>
2022-01-28 14:10:33 -0500

    Update locations.validations.js

--- src/validations/locations.validations.js
+++ src/validations/locations.validations.js
@@ -4,5 +4,6 @@ export const locationSchema = Joi.object({
   id: Joi.string().optional(),
   country: Joi.string().required(),
   inCountryRegion: Joi.string().required(),
+  // Please make 'inCountryRegion' optional.
   geographicIdentifier: Joi.string().required(),
 });
