f05d3dc814088f7d855f166dd4901d4be3c0a99f
Michael Taylor <mtaylor@michaeltaylor.dev>
2022-02-07 12:14:25 -0500

    fix: fk uuid types from integer to string for uuid

--- src/models/locations/locations.modeltypes.cjs
+++ src/models/locations/locations.modeltypes.cjs
@@ -10,7 +10,7 @@ module.exports = {
     primaryKey: true,
   },
   warehouseProjectId: {
-    type: Sequelize.INTEGER,
+    type: Sequelize.STRING,
     required: true,
     onDelete: 'CASCADE',
   },
