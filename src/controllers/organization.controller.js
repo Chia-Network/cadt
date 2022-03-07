99b226265de3d1181903cc8af4087a43f29198dc
Michael Taylor <mtaylor@michaeltaylor.dev>
2022-03-07 18:16:39 -0500

    feat: upload svg icon

--- src/controllers/organization.controller.js
+++ src/controllers/organization.controller.js
@@ -4,6 +4,7 @@ import {
   assertHomeOrgExists,
   assertWalletIsSynced,
   assertDataLayerAvailable,
+  assertWalletIsAvailable,
   assertIfReadOnlyMode,
 } from '../utils/data-assertions';
 
@@ -11,10 +12,43 @@ export const findAll = async (req, res) => {
   return res.json(await Organization.getOrgsMap());
 };
 
+export const createV2 = async (req, res) => {
+  try {
+    await assertIfReadOnlyMode();
+    await assertDataLayerAvailable();
+    await assertWalletIsAvailable();
+    await assertWalletIsSynced();
+
+    const myOrganization = await Organization.getHomeOrg();
+
+    if (!myOrganization) {
+      return res.json({
+        message: 'Your organization already exists.',
+        orgId: myOrganization.orgUid,
+      });
+    } else {
+      const { name } = req.body;
+      const buffer = req.files.svg.data;
+      const svgIcon = buffer.toString('utf8');
+
+      return res.json({
+        message: 'New organization created successfully.',
+        orgId: await Organization.createHomeOrganization(name, svgIcon, 'v1'),
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: 'Error initiating your organization',
+      error: error.message,
+    });
+  }
+};
+
 export const create = async (req, res) => {
   try {
     await assertIfReadOnlyMode();
     await assertDataLayerAvailable();
+    await assertWalletIsAvailable();
     await assertWalletIsSynced();
 
     const myOrganization = await Organization.getHomeOrg();
