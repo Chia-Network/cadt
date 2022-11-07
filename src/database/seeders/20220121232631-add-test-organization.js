d63212ceedb80caca3ec7cfaf9de4d7f25b2365e
Zachary Brown <z.brown@chia.net>
2022-11-07 15:20:57 -0800

    assert { type: 'json' } all over

--- src/database/seeders/20220121232631-add-test-organization.js
+++ src/database/seeders/20220121232631-add-test-organization.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import OrganizationStub from '../../models/organizations/organizations.stub.json';
+import OrganizationStub from '../../models/organizations/organizations.stub.json' assert { type: 'json' };
 
 export default {
   up: async (queryInterface) => {
