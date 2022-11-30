276608826810b027492f7ffd1a39227f4d8436d4
Chris Marslender <chrismarslender@gmail.com>
2022-11-30 15:22:13 -0600

    Get chia root from env if set, otherwise fall back to ~/.chia/mainnet

---
+++ src/utils/chia-root.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+
+import os from 'os';
+import path from 'path';
+
+export const getChiaRoot = _.memoize(() => {
+    let chiaRoot;
+
+    if(process.env.CHIA_ROOT) {
+        chiaRoot = path.resolve(process.env.CHIA_ROOT)
+    } else {
+        const homeDir = os.homedir();
+        chiaRoot = path.resolve(`${homeDir}/.chia/mainnet`);
+    }
+
+    return chiaRoot
+});
