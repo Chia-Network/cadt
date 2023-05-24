11c719cafda1d05bbafc78583a5c3e40d3dffa8f
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-05-24 12:09:58 -0400

    fix: make sure all posts have a send

--- src/datalayer/fullNode.js
+++ src/datalayer/fullNode.js
@@ -1,49 +1,8 @@
 import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
-import request from 'request-promise';
-import { getConfig } from '../utils/config-loader';
 import yaml from 'js-yaml';
 import { getChiaRoot } from '../utils/chia-root.js';
-
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
-
-const rpcUrl = getConfig().APP.WALLET_URL;
-
-const getBaseOptions = () => {
-  const chiaRoot = getChiaRoot();
-  const certFile = path.resolve(
-    `${chiaRoot}/config/ssl/full_node/private_full_node.crt`,
-  );
-  const keyFile = path.resolve(
-    `${chiaRoot}/config/ssl/full_node/private_full_node.key`,
-  );
-
-  const baseOptions = {
-    method: 'POST',
-    cert: fs.readFileSync(certFile),
-    key: fs.readFileSync(keyFile),
-  };
-
-  return baseOptions;
-};
-
-const getActiveNetwork = async () => {
-  const options = {
-    url: `${rpcUrl}/get_network_info`,
-    body: JSON.stringify({}),
-  };
-
-  const response = await request(Object.assign({}, getBaseOptions(), options));
-
-  const data = JSON.parse(response);
-
-  if (data.success) {
-    return data;
-  }
-
-  return false;
-};
 
 export const getChiaConfig = _.memoize(() => {
   const chiaRoot = getChiaRoot();
@@ -53,6 +12,5 @@ export const getChiaConfig = _.memoize(() => {
 });
 
 export default {
-  getActiveNetwork,
   getChiaConfig,
 };
