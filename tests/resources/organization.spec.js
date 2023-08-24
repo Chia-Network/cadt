b7e5d1005b9b914682fd73aa15c4a5e8e722d0cf
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-08-24 16:57:30 -0400

    fix: tests

--- tests/resources/organization.spec.js
+++ tests/resources/organization.spec.js
@@ -52,8 +52,10 @@ describe('Orgainzation Resource CRUD', function () {
       );
     }).timeout(TEST_WAIT_TIME * 10);
 
-    it.only('Organization can be retreived from datalayer', async function () {
+    it('Organization can be retreived from datalayer', async function () {
       const response = await supertest(app).get(`/v1/organizations`).send();
+
+      console.log(response.body);
 
       expect(Object.values(response.body)[0].name).to.equal('My Org');
       expect(Object.values(response.body)[0].icon).to.equal(
