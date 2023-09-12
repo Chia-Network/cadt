935fede8cac21a6f2378b3c600cf94faf6885b47
Michael Taylor <mtaylor@michaeltaylor.dev>
2023-09-12 14:21:45 -0400

    refactor: move to top of controller

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -127,6 +127,22 @@ export const findAll = async (req, res) => {
       order,
     } = req.query;
 
+    if (!page) {
+      let redirectUrl = `${req.baseUrl}?page=1&limit=10`;
+
+      if (Object.keys(req.query).length > 0) {
+        redirectUrl = redirectUrl.concat(
+          '&',
+          Object.keys(req.query)
+            .map((key) => `${key}=${req.query[key]}`)
+            .join('&'),
+        );
+      }
+
+      // Redirect to the modified URL
+      return res.redirect(301, redirectUrl);
+    }
+
     let where = orgUid != null && orgUid !== 'all' ? { orgUid } : undefined;
 
     if (filter) {
@@ -171,22 +187,6 @@ export const findAll = async (req, res) => {
     }
 
     let pagination = paginationParams(page, limit);
-
-    if (!page) {
-      let redirectUrl = `${req.baseUrl}?page=1&limit=10`;
-
-      if (Object.keys(req.query).length > 0) {
-        redirectUrl = redirectUrl.concat(
-          '&',
-          Object.keys(req.query)
-            .map((key) => `${key}=${req.query[key]}`)
-            .join('&'),
-        );
-      }
-
-      // Redirect to the modified URL
-      return res.redirect(301, redirectUrl);
-    }
 
     if (xls) {
       pagination = { page: undefined, limit: undefined };
