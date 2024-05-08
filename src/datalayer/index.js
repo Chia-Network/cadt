35230b9d86c628487637009db0e975f3f97d2bf9
Michael Taylor <mtaylor@michaeltaylor.dev>
2024-05-07 20:47:54 -0400

    fix: missing function

--- src/datalayer/index.js
+++ src/datalayer/index.js
@@ -2,10 +2,12 @@ import writeService from './writeService';
 import syncService from './syncService';
 import wallet from './wallet';
 import fullNode from './fullNode';
+import persistance from './persistance';
 
 export default {
   ...writeService,
   ...syncService,
   ...wallet,
   ...fullNode,
+  ...persistance,
 };
