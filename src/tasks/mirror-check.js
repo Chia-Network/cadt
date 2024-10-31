35fda9a074dfe5a386994ca643678819a3d683c5
William Wills <wwills2@protonmail.com>
2024-10-31 12:41:33 -0400

    feat: mirror check task automatically adds missing governance mirrors

--- src/tasks/mirror-check.js
+++ src/tasks/mirror-check.js
@@ -8,9 +8,9 @@ import { logger } from '../logger';
 import { CONFIG } from '../user-config';
 import { getMirrorUrl } from '../utils/datalayer-utils';
 import dotenv from 'dotenv';
+import datalayer from '../datalayer';
 
 dotenv.config();
-
 // This task checks if there are any mirrors that have not been properly mirrored and then mirrors them if not
 
 const task = new Task('mirror-check', async () => {
@@ -45,48 +45,62 @@ const job = new SimpleIntervalJob(
 
 const runMirrorCheck = async () => {
   const mirrorUrl = await getMirrorUrl();
+  const GOVERNANCE_BODY_ID = CONFIG()?.CADT?.GOVERNANCE?.GOVERNANCE_BODY_ID;
 
-  if (mirrorUrl) {
-    const governanceOrgUidResult = await Meta.findOne({
-      where: { metaKey: 'governanceBodyId' },
-      attributes: ['metaValue'],
-      raw: true,
-    });
-    const governanceRegistryIdResult = await Meta.findOne({
-      where: { metaKey: 'mainGoveranceBodyId' },
-      attributes: ['metaValue'],
-      raw: true,
-    });
-
-    if (
-      governanceOrgUidResult?.metaValue &&
-      governanceRegistryIdResult?.metaValue
-    ) {
-      // There is logic within the addMirror function to check if the mirror already exists
-      await Organization.addMirror(
-        governanceOrgUidResult?.metaValue,
-        mirrorUrl,
-        true,
-      );
-      await Organization.addMirror(
-        governanceRegistryIdResult?.metaValue,
-        mirrorUrl,
-        true,
-      );
-    }
-
-    const organizations = await Organization.getOrgsMap();
-    const orgs = Object.keys(organizations);
-    for (const org of orgs) {
-      const orgData = organizations[org];
-      // There is logic within the addMirror function to check if the mirror already exists
-      await Organization.addMirror(orgData.orgUid, mirrorUrl, true);
-      await Organization.addMirror(orgData.registryId, mirrorUrl, true);
-    }
-  } else {
+  if (!mirrorUrl) {
     logger.info(
       'DATALAYER_FILE_SERVER_URL not set, skipping mirror announcements',
     );
+    return;
+  }
+
+  // get governance info if governance node
+  const governanceOrgUidResult = await Meta.findOne({
+    where: { metaKey: 'governanceBodyId' },
+    attributes: ['metaValue'],
+    raw: true,
+  });
+  const governanceRegistryIdResult = await Meta.findOne({
+    where: { metaKey: 'mainGoveranceBodyId' },
+    attributes: ['metaValue'],
+    raw: true,
+  });
+
+  if (
+    governanceOrgUidResult?.metaValue &&
+    governanceRegistryIdResult?.metaValue
+  ) {
+    // add governance mirrors if instance is governance
+    // There is logic within the addMirror function to check if the mirror already exists
+    await Organization.addMirror(
+      governanceOrgUidResult?.metaValue,
+      mirrorUrl,
+      true,
+    );
+    await Organization.addMirror(
+      governanceRegistryIdResult?.metaValue,
+      mirrorUrl,
+      true,
+    );
+  } else if (GOVERNANCE_BODY_ID) {
+    const governanceStoreValue =
+      await datalayer.getSubscribedStoreData(GOVERNANCE_BODY_ID);
+
+    if (governanceStoreValue?.v1) {
+      // add governance mirrors if non-governance instance
+      await Organization.addMirror(GOVERNANCE_BODY_ID, mirrorUrl, true);
+      await Organization.addMirror(governanceStoreValue.v1, mirrorUrl, true);
+    } else {
+      logger.warn('error adding governance mirrors');
+    }
+  }
+
+  const organizations = await Organization.getOrgsMap();
+  const orgs = Object.keys(organizations);
+  for (const org of orgs) {
+    const orgData = organizations[org];
+    await Organization.addMirror(orgData.orgUid, mirrorUrl, true);
+    await Organization.addMirror(orgData.registryId, mirrorUrl, true);
   }
 };
 
