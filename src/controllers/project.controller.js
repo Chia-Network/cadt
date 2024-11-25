8c53345b9bdc0cbd1cea1e020e42c47ab4c907c5
William Wills <wwills2@protonmail.com>
2024-11-25 12:00:34 -0500

    fix: failing tests

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -461,7 +461,7 @@ export const destroy = async (req, res) => {
     await Staging.create(stagedData);
 
     res.json({
-      message: 'Project deleted staged successfully',
+      message: 'Project deletion staged successfully',
       success: true,
     });
   } catch (err) {
