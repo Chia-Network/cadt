3da7b68ca2080d77380906f369f7635151a2da2e
William Wills <wwills2@protonmail.com>
2025-01-24 12:11:19 -0500

    fix: default sync task ending before governance data available

--- src/tasks/sync-default-organizations.js
+++ src/tasks/sync-default-organizations.js
@@ -17,12 +17,6 @@ const task = new Task('sync-default-organizations', async () => {
 
     if (!CONFIG.USE_SIMULATOR) {
       const defaultOrgRecords = await getDefaultOrganizationList();
-      if (!Array.isArray(defaultOrgRecords)) {
-        throw new Error(
-          'ERROR: Default Organization List Not found, This instance may be missing data from default orgs',
-        );
-      }
-
       const userDeletedOrgs = await Meta.getUserDeletedOrgUids();
 
       for (const { orgUid } of defaultOrgRecords) {
